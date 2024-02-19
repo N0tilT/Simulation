72639bd520fa0eeb8753de39a9ec731c12d5f358
Timofey Latypov <timtompson3241@gmail.com>
2024-02-19 15:15:21 +0300

    refactoring + randomizer normalization

--- Lab2/randomizer.py
+++ Lab2/randomizer.py
@@ -9,18 +9,24 @@ class Random:
     def __init__(self):
         Random.x = time.time()
 
-    def next(self):
-        
-        Random.x = int((Random.a * Random.x)%Random.m)
-        return Random.x
+    def nextDouble(self,leftBorder,rightBorder):
+        Random.x  = (Random.a * Random.x)%Random.m
+        return (Random.x / Random.m) * (rightBorder - leftBorder) + leftBorder
+    
+    def nextInt(self,leftBorder,rightBorder):
+        return int(self.nextDouble(leftBorder,rightBorder))
  
 random = Random()
+
 values = []
-signal_amount = 10000
+signal_amount = 500
+minimumValueOfRandom = 0
+maximumValueOfRandom = 1
+step = maximumValueOfRandom/25
 
 for i in range(signal_amount):
 
-    values.append(random.next())
+    values.append(random.nextDouble(minimumValueOfRandom,maximumValueOfRandom))
 
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
@@ -28,27 +34,35 @@ plt.rcParams["figure.autolayout"] = True
 x = np.array([x for x in range(signal_amount)])
 y = np.array(values)
 
-print(values)
-
 expected_val = sum(values)/signal_amount
 dispersion = sum(list(map(lambda x: (x-expected_val) ** 2,values)))/signal_amount
 standart_deviation = dispersion**0.5
-
+print(f"Количество чисел равно {signal_amount}")
+print("---------------")
+print("Статистический тест")
+print("Мат. ожидание:")
 print(expected_val)
+print("Дисперсия:")
 print(dispersion)
+print("Ср.квадратичное:")
 print(standart_deviation)
 
-plt.title("Line graph")
-#plt.plot(x, y, color="violet")
+leftBorderIdeal = expected_val - standart_deviation
+rightBorderIdeal = expected_val + standart_deviation
 
+idealNumbers = [x for x in values if x>leftBorderIdeal and x<rightBorderIdeal]
+percentage = len(idealNumbers)/signal_amount * 100
+print("---------------")
+print("Частотный тест:")
+print(f"{percentage:.4f}% значений попали в отрезок ({leftBorderIdeal:.4f};{rightBorderIdeal:.4f})")
 
 # matplotlib histogram
-plt.hist(values, color = 'blue', edgecolor = 'black',
-         bins = int(Random.m/1680.7))
+plt.hist([values,idealNumbers], color = ['lightgreen','violet'], label=['Числа вне эталонного диапазона','Эталонный диапазон'],
+         bins = int(maximumValueOfRandom/step))
 
 # Add labels
-plt.title('Распределение случайных чесил')
-plt.xlabel('Количество')
-plt.ylabel('Отрезок')
-
+plt.title('Распределение случайных чисел')
+plt.ylabel('Количество')
+plt.xlabel('Отрезок')
+plt.legend()
 plt.show()
